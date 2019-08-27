35252eada701ac741840de7e06fa71ceb3a3a840
Al-Bsharat <qaisbsharat@onedrive.web.id>
2019-08-27 13:27:23 +0300

    Adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {CardList} from './components/card-list/card-list.component'
+import {SearchBox} from "./components/search-box/search-box.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            monsters: [],
+            searchField: ''
+        };
+    };
+
+    async componentDidMount() {
+        let objectSource = await fetch('https://jsonplaceholder.typicode.com/users');
+        let monsters = await objectSource.json();
+        this.setState({monsters: monsters});
+    }
+
+    render() {
+        const {monsters, searchField} = this.state; // Destructuring.
+        const filteredMonsters = monsters
+            .filter(m => m.name.toLowerCase().includes(searchField.toLowerCase()));
+
+        return (
+            <div className="App">
+                <h1>Monster Rolodex</h1>
+                <SearchBox
+                    placeholder='search monster'
+                    handelChange={e => this.setState({searchField: e.target.value})}/>
+                <CardList monsters={filteredMonsters}/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,1 @@
+import React from 'react';import './card-list.styles.css'import '../card/card.compontent'import {Card} from "../card/card.compontent";export const CardList = props => (        <div className='card-list'>            {                props.monsters.map(monster => (                    <Card key={monster.id} monster={monster}/>))            }        </div>    );
