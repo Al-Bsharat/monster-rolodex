32d8b98216d3b0ce0aa3ab9f1d01ece5c3a1ffc7
Al-Bsharat <qaisbsharat@onedrive.web.id>
2019-08-27 13:26:13 +0300

    Updates

---
+++ precache-manifest.1bbadabde7ca53494de15c709cc5889d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d9d1a75a3a54b3e3462701392cf83f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f11b73aa154b734c1353",
+    "url": "/monster-rolodex/static/css/main.2ed0d61f.chunk.css"
+  },
+  {
+    "revision": "1d6e91a32744dfc2484c",
+    "url": "/monster-rolodex/static/js/2.1e02ed1a.chunk.js"
+  },
+  {
+    "revision": "f11b73aa154b734c1353",
+    "url": "/monster-rolodex/static/js/main.fa6701e9.chunk.js"
+  },
+  {
+    "revision": "a0942a09f1e0d21c02b5",
+    "url": "/monster-rolodex/static/js/runtime~main.7288f465.js"
+  }
+]);
